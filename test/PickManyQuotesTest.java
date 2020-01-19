d8e4f3ae951fc1e3000b6483318e383447f9d8b0
Pablo Navajas Helguero <P.Navajas-Helguero@warwick.ac.uk>
2020-01-19 20:00:30 +0000

    Updated style in test files

--- test/PickManyQuotesTest.java
+++ test/PickManyQuotesTest.java
@@ -1,47 +1,41 @@
-import org.junit.Test;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.StringReader;
 import java.util.List;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
+import org.junit.Test;
 
 public class PickManyQuotesTest {
-
-
 
   @Test
   public void test() throws Exception {
 
-       final String quote = "What a test!;Anonymous";
-       final String reformattedQuote = "`What a test!' by Anonymous";
+    final String quote = "What a test!;Anonymous";
+    final String reformattedQuote = "`What a test!' by Anonymous";
 
-      String input = String.join("\n",
-              quote + " Line 1", quote + " Line 2", quote + " Line 3");
+    String input = String.join("\n",
+            quote + " Line 1", quote + " Line 2", quote + " Line 3");
 
-      BufferedReader in = new BufferedReader(new StringReader(input));
+    BufferedReader in = new BufferedReader(new StringReader(input));
 
-      List<Quote> quotes = PickManyQuotes.loadQuotes(in);
+    List<Quote> quotes = PickManyQuotes.loadQuotes(in);
 
-      assertThat(quotes.size(), is(3));
-      for(int i=0;i<3;i++) {
-          assertThat(quotes.get(i).toString(), containsString(reformattedQuote + " Line "+(i+1)));
-      }
+    assertThat(quotes.size(), is(3));
+    for (int i = 0; i < 3; i++) {
+      assertThat(quotes.get(i).toString(), containsString(reformattedQuote + " Line " + (i + 1)));
+    }
 
-      ByteArrayOutputStream outstream = new ByteArrayOutputStream();
-      System.setOut(new PrintStream(outstream));
+    ByteArrayOutputStream outstream = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outstream));
 
-      PickManyQuotes.reformat(quotes, new int[]{1});
+    PickManyQuotes.reformat(quotes, new int[]{1});
 
-      String output = outstream.toString();
+    String output = outstream.toString();
 
-      assertThat(output, containsString(reformattedQuote + " Line "+1));
-
+    assertThat(output, containsString(reformattedQuote + " Line " + 1));
   }
-
 }
